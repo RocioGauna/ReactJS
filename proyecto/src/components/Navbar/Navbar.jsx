4b14a95aff4d824e2c79e9c74a31c1ba0470c810
gauna_rocio <rociomgauna5@gmail.com>
2021-12-21 07:22:45 -0300

    Creando mi landing, añadiendo el icon del carrito de compras

--- proyecto/src/components/Navbar/Navbar.jsx
+++ proyecto/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon from './Icon'
 import './Navbar.css'
 
 function Navbar() {
@@ -9,6 +10,7 @@ function Navbar() {
                 <li className='navbar-li'>Personajes</li>
                 <li className='navbar-li'>Skins</li>
                 <li className='navbar-li'>Contacto</li>
+                <Icon />
             </ul>
         </nav>
     )
