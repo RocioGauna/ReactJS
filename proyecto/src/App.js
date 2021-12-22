74d47f74114efea8a9a59d2e3bd60c427bfa1f9e
gauna_rocio <rociomgauna5@gmail.com>
2021-12-21 21:48:31 -0300

    ItemListContainer

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
+  const saludar = 'Hola'
+
   return (
-    <Navbar />
+    <center>
+      <Navbar />
+      <ItemListContainer greeting = {saludar} />
+    </center>
   );
 }
 
